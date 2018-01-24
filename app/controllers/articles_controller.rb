03a4d8dc13628e3737ca903b3d4ea5d30814d655
brett <brettlorsch@gmail.com>
2018-01-23 23:16:58 -0700

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  
+ def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  # snippet for brevity
+  
+def create
+  @article = Article.new(params.require(:article).permit(:title, :text))
+ 
+  @article.save
+  redirect_to @article
+end
+
+private
+def article_params
+  params.require(:article) .permit(:title, :text)
+  end
+end
